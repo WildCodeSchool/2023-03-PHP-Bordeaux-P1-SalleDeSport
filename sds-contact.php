dc770323362d2ed10e15739aca539bde766bf2c1
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-03-15 15:24:30 +0100

    mise à jour pour fonctionnement css

--- sds-contact.php
+++ sds-contact.php
@@ -1,6 +1,18 @@
 <?php
 include "_includes/sds-header.php";
 ?>
+<div class="container-contact"></div>
+<h2>Pour nous contacter</h2>
+<input type="text" placeholder="Name"><br>
+<input type="email" placeholder="mon.email@exemple.com"><br>
+<input type="textarea"><br>
+<bouton>Submit</bouton>
+<div class="underline"></div>
+<img src="#" alt="Map">
+<p>Adresse</p>
+<p> C'est par là</p>
+<p> 33000 Bordeaux</p>
+</div>
 
 
 
