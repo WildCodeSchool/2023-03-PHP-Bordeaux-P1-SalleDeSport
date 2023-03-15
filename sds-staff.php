61fc0fcfa974442ecb04b7e1c4b2e52e37d0e841
Aline LEGENDRE <legendrealine@gmail.com>
2023-03-15 14:41:18 +0100

    added html sds-staff and create branch Aline

--- sds-staff.php
+++ sds-staff.php
@@ -2,7 +2,20 @@
 include "_includes/sds-header.php"
 ?>
 
-
+<div>
+    <div class="card1">
+        <img src="assets/images/prof1.jpg">
+        <p>Alexandra</p>
+    </div>
+    <div class="card2">
+        <img src="assets/images/prof2.jpg">
+        <p>Youssouf</p>
+    </div>
+    <div class="card3">
+        <img src="assets/images/prof3.jpeg">
+        <p>Aline</p>
+    </div>
+</div>
 
 <?php
 include "_includes/sds-footer.php"
