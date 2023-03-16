8a8fe1dfaec441ee68a6a7192b9e9a72b65daacf
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-03-16 09:14:04 +0100

    update maps

--- sds-contact.php
+++ sds-contact.php
@@ -11,7 +11,7 @@ include "_includes/sds-header.php";
             <input class="submitMessage" type="submit" value="Envoyer">
         </form>
         <div class="container-address">
-            <img src="assets/images/Map.png" alt="Map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.6734872846687!2d-0.5669889844913534!3d44.868941480984944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527cba74d1bdf%3A0xb92bcdcad0920688!2sWild%20Code%20School!5e0!3m2!1sfr!2sfr!4v1678953980112!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <h3>Adresse</h3>
             <p>171 Rue Lucien Faure</p>
             <p> 33000 Bordeaux</p>
