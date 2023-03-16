a710a047fe53e6677c6458736590a42240365e83
Abdulkader <youssouf.soudjay@gmail.com>
2023-03-16 09:08:53 +0100

    update sds home

--- sds-home.php
+++ sds-home.php
@@ -1,33 +1,41 @@
 <?php
-include "header.php";
+include "_includes/sds-header.php";
 ?>
-<nav>
-<img src="background.jpg" alt="image head">
-<img src="logo.png" alt="logo">
-       </nav>
-<h1>Lorem fhlkthpo kdfgsdgl</h1>
+<main id="main/home">
+    <nav class="image">
+<img src="images/background.jpg" alt="image" class="image">
+    </nav>
+ 
+    
 <div>
+    <h1>Lorem fhlkthpo </h1>
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus saepe, ipsum ab magnam vitae laudantium commodi qui! Accusamus nam eius laboriosam eos quidem, assumenda voluptatum laborum qui est minus!
         Ducimus culpa laudantium nisi error magnam modi, rerum quos, et laboriosam ab dolor quae nemo nam perspiciatis at sit totam alias consequuntur reprehenderit unde rem quasi nobis maiores? Eligendi, iste.
         Repudiandae facere doloremque illo velit alias delectus id minima possimus non. In voluptatem, repudiandae possimus minima optio praesentium delectus repellat maxime quaerat, ad quibusdam porro nulla officia qui non illo?
-        Suscipit ipsum illum distinctio sapiente minus ab labore, veritatis dolorum hic explicabo, sit porro ducimus delectus? Dolore laudantium reprehenderit nemo, in asperiores odio, quasi quis enim quisquam, mollitia tempore ratione.
-        Architecto aut perferendis error suscipit accusamus minus eligendi maxime quibusdam ut doloremque mollitia fugit corrupti esse quisquam in quam distinctio, et velit nulla dolores ipsa facere optio! Ab, sequi soluta.
-        Error a ex perferendis qui. Adipisci ab quae neque ex facere quia eveniet quisquam natus, cumque impedit fugit porro, sequi unde nam vitae possimus accusamus atque suscipit eligendi error distinctio?
-        Temporibus ex eaque quasi libero earum voluptates id praesentium eos, provident ducimus cum quis dignissimos modi dolorem fuga. Ipsa ipsum nihil tempora dignissimos commodi excepturi eum perspiciatis sapiente repellendus a.
-        Eaque vel amet tempore, minus architecto non recusandae voluptatibus accusantium! Voluptatem eligendi nobis recusandae culpa. Libero, harum? Modi voluptas libero voluptates soluta repellendus fuga necessitatibus at blanditiis aspernatur tempora. Delectus!
-        Reprehenderit accusantium iure at voluptas recusandae possimus nostrum. Quam placeat maiores ratione! Iure quo cupiditate iste commodi corporis, a minus obcaecati consequuntur sit quisquam, cumque nulla veritatis recusandae et aut?
-        Cupiditate cum nemo consectetur delectus corporis maiores consequatur architecto animi natus fugiat iure quis cumque odit eligendi dicta enim dolor, quod, ad dolores, accusamus tempore perspiciatis! Dignissimos veniam quaerat natus.
-    </p>
+     </p>
+
+<img src="images/imgArticle .png" alt="image article">
 </div>
+      
+      <h1>Retour d'expérience</h1>
 
 
-    
+    <div class="section">
+        <section><article><img src="images/firstCom.png" alt="first comment"class="photo"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde sequi reiciendis fugit veniam repellat deleniti sunt inventore pariatur cum iure odit, harum nam fuga, ullam asperiores aut numquam blandi</p></article></section>
+
+        <section><article><img src="images/secondCom.png" alt="second comment"class="photo"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, corrupti perferendis natus iure provident quis, dicta blanditiis nesciunt rem dolor, quam laborum rerum nobis a unde in repellendus. </p></article></section>
+
+        <section><article><img src="images/thirdCom.png" alt="third comment"class="photo"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorem praesentium dicta velit animi voluptatem ullam temporibus modi perferendis. Provident necessitatibus quidem atque dolores eaque</p></article></section>
+
+        <section><article><img src="images/fourthCom.png" alt="fourth comment"class="photo"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus fugit aperiam hic omnis numquam nostrum quo accusantium, debitis itaque explicabo amet. Alias reprehenderit, adipisci maxime </p></article></section>
+      
+    </div>
 
 
-
+</main>
 <?php
-include "footer.php";
+include "_/sds-footer.php";
 ?>
   
 
