f06cc6d260a3689bebcffae43eabe6cd554c4948
Gibon_david <[My\_gibon.david@live.fr](mailto:MY_gibon.david@live.fr)>
2023-03-21 14:12:28 +0100

    update layout  ination home

--- sds-home.php
+++ sds-home.php
@@ -2,62 +2,61 @@
 include "_includes/sds-header.php";
 ?>
 
- <main id="main" class="home">
-     <div class="imgbackground"></div>
+<main id="main" class="home">
+    <div class="imgbackground"></div>
 
-     <h1>Body and soul</h1>
-<div class="article">
-    <p>
-     Lorem ipsum dolor sit amet, consectetur adipisicing
-     elit. Aspernatur cum dicta ipsum molestiae odio
-     quibusdam repellendus soluta vel. Aperiam dignissimos 
-     illo ipsum libero nemo odio quisquam quo ullam, ut voluptate?
-     Lorem ipsum dolor sit amet, consectetur adipisicing 
-     elit. Aspernatur cum dicta ipsum molestiae odio quibusdame?
-    </p>
+    <h1>Body and soul</h1>
+    <div class="article">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Aspernatur cum dicta ipsum molestiae odio
+            quibusdam repellendus soluta vel. Aperiam dignissimos
+            illo ipsum libero nemo odio quisquam quo ullam, ut voluptate?
+            Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Aspernatur cum dicta ipsum molestiae odio quibusdame?
+        </p>
 
-<img src="assets/images/imgArticle.png" alt="image_article" class="image-article">
-</div>
-
+        <img src="assets/images/imgArticle.png" alt="image_article" class="image-article">
+    </div>
 
 
     <div class="section">
-    <h2 class="experienceTitle">Retour d'expérience</h2>
+        <h2 class="experienceTitle">Retour d'expérience</h2>
 
         <div class="card-home">
 
-        <img src="assets/images/firstCom.png" alt="first comment"class="photo">
-           <p class="text" > llat deleniti sunt inventore pariatur cum iure odit, 
-        pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicing 
-           </p>
-       
-        </div>
-        
-        <div class="card-home">
-        
-        <img src="assets/images/secondCom.png" alt="second comment"class="photo">
-           <p class="text" >Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicing
-           </p>
-       
-        </div>
-        
-        <div class="card-home">
-        
-        <img src="assets/images/thirdCom.png" alt="third comment"class="photo">
-           <p class="text" >Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicing 
-           </p>
-       
-        </div>
-        
-        <div class="card-home">
-        
-        <img src="assets/images/fourthCom.png" alt="fourth comment"class="photo">
-            <p class="text" >Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicin
+            <img src="assets/images/firstCom.png" alt="first comment" class="photo">
+            <p class="text"> llat deleniti sunt inventore pariatur cum iure odit,
+                pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicing
             </p>
-             
+
+        </div>
+
+        <div class="card-home">
+
+            <img src="assets/images/secondCom.png" alt="second comment" class="photo">
+            <p class="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicing
+            </p>
+
+        </div>
+
+        <div class="card-home">
+
+            <img src="assets/images/thirdCom.png" alt="third comment" class="photo">
+            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicing
+            </p>
+
+        </div>
+
+        <div class="card-home">
+
+            <img src="assets/images/fourthCom.png" alt="fourth comment" class="photo">
+            <p class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                pudila officia qui non illo?Lorem ipsum, dolor sit amet consectetur adipisicin
+            </p>
+
         </div>
     </div>
 
