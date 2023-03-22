7edaa5338f50f79192cf4b6ccb387519ff21cd3a
Aline LEGENDRE <legendrealine@gmail.com>
2023-03-22 19:20:28 +0100

    added staff description

--- sds-staff.php
+++ sds-staff.php
@@ -13,7 +13,7 @@ include "_includes/sds-header.php"
             <div class="card-back">
                 <img class="card-img-back" src="assets/images/prof1.jpg" alt="photo du professeur de sport Louis">
                 <h3>Louis</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam error inventore iusto, minus nihil qui ut? Distinctio dolorem fugiat hic id illum in ipsa itaque minus natus nihil.</p>
+                <p>Coach passionné, motivant et compétent. Louis vous aide à atteindre vos objectifs. Il propose des séances personnalisées pour un bien-être physique et mental.</p>
             </div>
         </div>
         <div class="card-staff">
@@ -24,7 +24,7 @@ include "_includes/sds-header.php"
             <div class="card-back">
                 <img class="card-img-back" src="assets/images/prof2.jpg" alt="photo du professeur de sport Marie">
                 <h3>Marie</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam error inventore iusto, minus nihil qui ut? Distinctio dolorem fugiat hic id illum in ipsa itaque minus natus nihil.</p>
+                <p>Guide experte en yoga pour une pratique équilibrante et apaisante. Marie propose des cours personnalisés pour une transformation intérieure et une meilleure santé.</p>
             </div>
         </div>
         <div class="card-staff">
@@ -35,7 +35,7 @@ include "_includes/sds-header.php"
             <div class="card-back">
                 <img class="card-img-back" src="assets/images/prof3.jpeg" alt="photo du professeur de sport Aurore">
                 <h3>Aurore</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam error inventore iusto, minus nihil qui ut? Distinctio dolorem fugiat hic id illum in ipsa itaque minus natus nihil.</p>
+                <p>Coach dynamique, motivante et expérimentée. Aurore propose des séances variées et adaptées à votre niveau pour un corps en forme et en bonne santé.</p>
             </div>
         </div>
     </div>
