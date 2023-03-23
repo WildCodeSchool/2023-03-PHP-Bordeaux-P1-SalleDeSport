87cebfab900af3ca3f8e7ebf6ea999ea93894aa6
chado <a.sale@outlook.fr>
2023-03-23 13:44:54 +0100

    prefixer css for adaptation multi browser on 4 last versions

--- _includes/scriptPull.php
+++ _includes/scriptPull.php
@@ -1,13 +1,15 @@
 <?php
 include "scriptDB.php";
+
 // RECUPERATION DES DONNEES DE LA DB ET INTEGRATION DE CARD HTML
 $sqlDrop = "SELECT nom, prenom, message ,img FROM fiche_client ORDER BY id DESC  LIMIT 4";
 $result = $conn->query($sqlDrop);
 
+// CREATION DE LA CARD COMMENTAIRE
 while($row = $result ->fetch_assoc()){
     echo '<div class="card-home">';
     echo '<img src="' . $row['img'] . '" alt="fourth comment" class="photo">';
-    echo '<div><p></p><strong>' . $row["nom"] . '  ' .$row["prenom"].' </strong><p/>';
+    echo '<div style="text-align: center;"><p></p><strong>' . $row["nom"] . '  ' .$row["prenom"].' </strong><p/>';
     echo '<p class="text">' . $row["message"] . '</p></div>';
     echo '</div>';
 };
--- _includes/sds-footer.php
+++ _includes/sds-footer.php
@@ -4,9 +4,10 @@
                                 <a href="">Les mentions légales</a>
                                 <a href="">Réglement interieur</a>
                     </div>
+
                     <div class="external-link part-of-footer" id="copyright">
                                <p>© Body&Soul.</p>
-                                <p>Made by Vox-Machina team.</p>
+                                <a href="">Made by Vox-Machina team.</a>
                     </div>
 
                     <div class="part-of-footer footer-contact">
@@ -14,7 +15,6 @@
                                 <div class="address footerFlex iconeCircle">
                                     <a href="../sds-contact.php">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                             <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
                                         </svg>
                                     </a>
@@ -24,7 +24,6 @@
                                 <div class="tel footerFlex iconeCircle">
                                     <a href="tel:0523456789" class="phone-button ">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                             <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
                                         </svg>
                                     </a>
