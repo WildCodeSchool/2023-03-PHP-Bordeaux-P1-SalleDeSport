ce991ba5b838aadcc03889d5621562589f565630
chado <a.sale@outlook.fr>
2023-03-23 18:31:23 +0100

    fix comments for img random

--- _includes/scriptPull.php
+++ _includes/scriptPull.php
@@ -2,14 +2,14 @@
 include "scriptDB.php";
 
 // RECUPERATION DES DONNEES DE LA DB ET INTEGRATION DE CARD HTML
-$sqlDrop = "SELECT nom, prenom, message ,img FROM fiche_client ORDER BY id DESC  LIMIT 4";
+$sqlDrop = "SELECT nom, prenom, message FROM fiche_client ORDER BY id DESC  LIMIT 4";
 $result = $conn->query($sqlDrop);
 
 // CREATION DE LA CARD COMMENTAIRE
-while($row = $result ->fetch_assoc()){
-    echo '<div class="card-home">';
-    echo '<img src="' . $row['img'] . '" alt="fourth comment" class="photo">';
-    echo '<div style="text-align: center;"><p></p><strong>' . $row["nom"] . '  ' .$row["prenom"].' </strong><p/>';
-    echo '<p class="text">' . $row["message"] . '</p></div>';
-    echo '</div>';
-};
+    while($row = $result ->fetch_assoc()){
+        echo '<div class="card-home">';
+        echo '<img src="https://picsum.photos/200?rand=' . rand() . '" alt="fourth comment" class="photo">';
+        echo '<div style="text-align: center;"><p></p><strong>' . $row["nom"] . '  ' .$row["prenom"].' </strong><p/>';
+        echo '<p class="text">' . $row["message"] . '</p></div>';
+        echo '</div>';
+    };
